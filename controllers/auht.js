5d9c304e97fa88dd58e57e435e14a2fba4fefa8a
keremcankurt <keremkurt278@gmail.com>
2024-05-19 14:40:27 +0300

    first commit

--- controllers/auht.js
+++ controllers/auht.js
@@ -29,7 +29,7 @@ const register = async (req, res, next) => {
             body { font-family: Arial, sans-serif; background-color: #f4f4f4; margin: 0; padding: 0; }
             .email-container { max-width: 600px; margin: 0 auto; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); }
             .header { background-color: #4CAF50; padding: 10px; text-align: center; border-radius: 8px 8px 0 0; }
-            .header img { max-width: 100px; margin-bottom: 10px; }
+            .header img { max-width: 100px; margin-bottom: 10px; border-radius: 50%; }
             .header h1 { color: #ffffff; margin: 0; font-size: 24px; }
             .content { padding: 20px; text-align: left; line-height: 1.6; }
             .content h2 { color: #333333; }
@@ -77,7 +77,7 @@ const register = async (req, res, next) => {
               body { font-family: Arial, sans-serif; background-color: #f4f4f4; margin: 0; padding: 0; }
               .email-container { max-width: 600px; margin: 0 auto; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); }
               .header { background-color: #4CAF50; padding: 10px; text-align: center; border-radius: 8px 8px 0 0; }
-              .header img { max-width: 100px; margin-bottom: 10px; }
+              .header img { max-width: 100px; margin-bottom: 10px; border-radius: 50%; }
               .header h1 { color: #ffffff; margin: 0; font-size: 24px; }
               .content { padding: 20px; text-align: left; line-height: 1.6; }
               .content h2 { color: #333333; }
